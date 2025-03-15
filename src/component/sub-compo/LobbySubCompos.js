ba88c147e85ed9a9b9e338106d0d071543a738b5
Leafurya <audrb5300@naver.com>
2025-03-15 11:04:08 +0900

    bug fix
    오늘 할 일 제목이 저장되는 버그 수정

--- src/component/sub-compo/LobbySubCompos.js
+++ src/component/sub-compo/LobbySubCompos.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import "../../style/Template.css"
 import "../../style/ProjectCardStyle.css"
 
@@ -12,7 +12,7 @@ function Task({task,idTree}){
 		<li>
 			<label className={`${check?" checked":""} task`}>
 				<input className='when_start' type="checkbox" defaultChecked={check} value={content} onChange={(event)=>{
-					ProjectModule.CheckItem(idTree[0],idTree[1]);
+					ProjectModule.CheckItem(idTree[0],idTree[1]); //group id, item id
 					ProjectModule.SavePorjects();
 					if(event.target.checked){
 						event.target.parentNode.classList.add("checked")
@@ -27,7 +27,7 @@ function Task({task,idTree}){
 		</li>
 	)
 }
-function ProjectCard({prjID}){
+function ProjectCard({prjID,refresher}){
 	let id=prjID;
 	let projectData=ProjectModule.GetProjectDataById(id);
 	let day=projectData.day;
@@ -35,9 +35,22 @@ function ProjectCard({prjID}){
 	let tasks=projectData.items;
 
 	const navigate=useNavigate();
-
 	return(
-		<li style={{marginTop:"7px"}} className="card " key={id}>
+		<li style={{marginTop:"7px",position:"relative"}} className="card " key={id}>
+			<input type="button" style={{
+				position:"absolute",
+				display:(id===0?"none":"inline-block"),
+				width:"25px",
+				top:0,
+				right:0,
+				textAlign:"center",
+				// background:"none",
+				border:"none"
+			}} value="↑" onClick={()=>{
+				ProjectModule.ToFront(id);
+				ProjectModule.SavePorjects();
+				refresher([]);
+			}}></input>
 			<header style={{display:"flex",flexDirection:"column"}}>
 				<div className="title" style={{display:"flex", flexDirection:"column", justifyContent:"space-between",padding:"15px 15px 5px 15px"}}>
 					<input id={`day${id}`} type="button" onClick={
@@ -101,6 +114,7 @@ function ProjectCard({prjID}){
 }
 
 function ProjectLists({prjIDs}){
+	const [_,refresh]=useState([]);
 	if(prjIDs===undefined){
 		return <ul className="project_list_ul">
 			loading
@@ -110,7 +124,7 @@ function ProjectLists({prjIDs}){
 	<ul className="project_list_ul">
 		{
 			prjIDs.map((id,index)=>{
-				return <ProjectCard key={index} prjID={id}></ProjectCard>
+				return <ProjectCard key={index} prjID={parseInt(id)} refresher={refresh}></ProjectCard>
 			})
 		}
 	</ul>
